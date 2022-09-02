7bd4a3ffc69a73af094c21830af95629a6e1f546
paul <pauledworthy@gmail.com>
2022-09-02 12:12:11 -0400

    style nav menu

--- app.py
+++ app.py
@@ -2,16 +2,15 @@ import os
 
 # remove at some point
 # from cs50 import SQL
-
 from flask import Flask, flash, redirect, render_template, request, session
-# from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
-# configure app
+# configure app (it tells the os the webapp starts here)
 app = Flask(__name__)
 
+# Ensure templates auto reload
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # config SQL to work with CS50 library
@@ -20,3 +19,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 # ensure API key is set
 if not os.environ.get("API_KEY"):
     raise RuntimeError("API_KEY not set")
+
+@app.route('/')
+def index():
+    current_user = session["user_id"]
